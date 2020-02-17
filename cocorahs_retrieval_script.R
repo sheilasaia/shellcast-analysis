577815f327bed763dffe8cae1e334e7bf1d18fe5
sheila <sms493@cornell.edu>
2020-02-17 15:46:14 -0500

    extract data from xml

--- cocorahs_retrieval_script.R
+++ cocorahs_retrieval_script.R
@@ -3,37 +3,74 @@
 # load libraries
 library(tidyverse)
 library(xml2)
+library(lubridate)
 # devtools::install_github("r-lib/xml2")
 # https://github.com/r-lib/xml2
 # helpful blog: https://blog.rstudio.com/2015/04/21/xml2/
 # more helpful blog: https://lecy.github.io/Open-Data-for-Nonprofit-Research/Quick_Guide_to_XML_in_R.html
 
-
 # example
-address <- "http://data.cocorahs.org/export/exportreports.aspx?ReportType=Daily&dtf=1&Format=XML&State=NC&ReportDateType=reportdate&Date=2/14/2020&TimesInGMT=False"	
+temp_address <- "http://data.cocorahs.org/export/exportreports.aspx?ReportType=Daily&dtf=1&Format=XML&State=NC&ReportDateType=reportdate&Date=2/14/2020&TimesInGMT=False"	
+temp_data <- xml2::download_xml(url = temp_address)
+temp_xml <- read_xml(temp_data)
 
-test <- xml2::download_xml(url = address)
-test_xml <- read_xml(test)
-test_structure <- xml_structure(test_xml)
-test_text <- xml_text(test_xml)
-test_name <- xml_name(test_xml) # from cocorahs
-test_children <- xml_children(test_xml)
-test_children2 <- xml_children(test_children[[1]]) # this is all the stations in NC (539 of them)
-test_children3 <- xml_children(test_children2[[1]]) # each of these is one of 13 variables for the first of the 539 sites
-# test_children4 <- xml_children(test_children3[[1]])
-# test_children3[[2]][1]
-xml_name(test_children2) # gives all report headers for each site...not too helpful
-xml_name(test_children3) # gives all headers for each report! (13 of them)
+# save data to lists
+# xml_child(xml_child((test_xml))) # see all report entries for one station
+# want to keep ObservationDate, ObservationTime, StationNumber, StationName, Latitude, Longitude, TotalPrecipAmt
+obs_date_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//ObservationDate"))
+obs_time_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//ObservationTime"))
+station_numbers_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//StationNumber"))
+station_name_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//StationName"))
+lat_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//Latitude"))
+long_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//Longitude"))
+precip_list <- xml_text(xml_find_all(xml_children(xml_children((temp_xml)))[1], "//TotalPrecipAmt")) # in inches (T = trace, NA = NA)
 
-# find station number nodes
-stations <- xml_find_all(test_children2, ".//StationNumber")
-xml_attr(stations, attr = )
+# make dataframe/tibble
+temp_data <- tibble(date = obs_date_list,
+                  time = as.character(obs_time_list),
+                  station_id = station_numbers_list,
+                  station_name = station_name_list,
+                  lat = as.numeric(lat_list),
+                  long = as.numeric(long_list),
+                  precip_in = as.numeric(precip_list)) %>% # as.numeric() will convert NAs and Ts all to NAs
+  na.omit() # delete NA entries
+# NA warning is ok here this happens from using as.numeric
+# map(temp_data, class) # checks classes of columns
+
+# for loop for multiple days
+# define start and end days
+start_date <- ymd("2016-01-01")
+end_date <- ymd("2016-01-14")#ymd("2018-12-31")
+day_step <- duration(num = 1, units = "days")
+num_days <- time_length(end_date - start_date, unit = "days")
+
+# make empty data frame/tibble
+cocorahs_data <- tibble(date = character(),
+                        time = character(),
+                        station_id = character(),
+                        station_name = character(),
+                        lat = numeric(),
+                        long = numeric(),
+                        precip_in = numeric())
+# map(cocorahs_data, class) # checks classes of columns
+
+# loop
+for (i in 0:num_days) {
+  # set-up date and save values
+  temp_date <- start_date + i*day_step
+  temp_day <- day(temp_date)
+  temp_month <- month(temp_date)
+  temp_year <- year(temp_date)
+  
+  # define url address
+  temp_address <- paste0("http://data.cocorahs.org/export/exportreports.aspx?ReportType=Daily&dtf=1&Format=XML&State=NC&ReportDateType=reportdate&Date=", temp_month, "/", temp_day, "/", temp_year, "&TimesInGMT=False")
+  
+  
+  
+  
+  
+}
 
 
-xml_parent(test_xml)
-xml_name(xml_parent(test_xml))
-xml_name(xml_children(xml_children(test_xml)))
-station1 <- xml_find_first(xml_children(xml_children(test_xml)), "//DailyPrecipReport" )
-xml_text()
-
-
+# TODO finish for loop!
+# TODO fix date time aspect of data
