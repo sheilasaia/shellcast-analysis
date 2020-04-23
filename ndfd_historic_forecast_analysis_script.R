1d890d340fe63e8ae85485ee301287ff5859cb5b
sheila <sms493@cornell.edu>
2020-04-23 19:55:59 -0400

    rasterize and add header

--- ndfd_historic_forecast_analysis_script.R
+++ ndfd_historic_forecast_analysis_script.R
@@ -1,15 +1,29 @@
-# ndfd historic forecast analysis script
 
-# ---- To Do List ----
+# script name: ndfd historic forecast analysis script
+# purpose of script: analyze historic ndfd data
+# author: sheila saia 
+# date created: 20200401
+# email: ssaia@nscu.edu
+
+
+# ----
+# notes:
+ 
+
+# ----
+# to do list
 # TODO for now set date as characture but figure out why ymd_hm("2016-01-01 00:00", tz = "UCT") gives result without time but ymd_hm("2016-01-01 13:00", tz = "UCT") gives it with the time
 # TODO need to loop through all 1095 days
 # TODO use here package for path stuff
+# TODO figure out how to get raster to be 5000x5000 right now if i set this when i make the empty raster, the final raster has lines in it
+
 
 # ---- load libraries ----
 library(tidyverse)
 library(here)
 library(lubridate)
 library(sf)
+library(raster)
 
 
 # ---- define paths ----
@@ -20,129 +34,210 @@ state_bounds_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellca
 # exporting figure path
 figure_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/results/figures/"
 
+# exporting spatial data path
+spatial_data_exort_path <- ""
+
 
 # ---- load data ----
 # historic pop12 data
-ndfd_hist_pop12_data_raw <- read_csv(paste0(ndfd_sco_data_path, "pop12_2016010100.csv"),
-                                     col_types = list(col_double(), col_double(), col_double(), col_double(), col_double(), col_double(), col_double(),
-                                                      col_character(), col_character(), col_character(), col_character(), col_character()))
+ndfd_pop12_data_raw <- read_csv(paste0(ndfd_sco_data_path, "pop12_2016010100.csv"),
+                                col_types = list(col_double(), col_double(), col_double(), col_double(), col_double(), col_double(), col_double(),
+                                                 col_character(), col_character(), col_character(), col_character(), col_character()))
 
 # historic qpf data
-ndfd_hist_qpf_data_raw <- read_csv(paste0(ndfd_sco_data_path, "qpf_2016010100.csv"),
-                                   col_types = list(col_double(), col_double(), col_double(), col_double(), col_double(), col_double(), col_double(),
-                                                    col_character(), col_character(), col_character(), col_character(), col_character()))
+ndfd_qpf_data_raw <- read_csv(paste0(ndfd_sco_data_path, "qpf_2016010100.csv"),
+                              col_types = list(col_double(), col_double(), col_double(), col_double(), col_double(), col_double(), col_double(),
+                                               col_character(), col_character(), col_character(), col_character(), col_character()))
 
 # state boundaries
-state_bounds_albers_shp <- st_read(paste0(state_bounds_data_path, "state_bounds.shp"))
+state_bounds_shp_raw <- st_read(paste0(state_bounds_data_path, "state_bounds.shp"))
+st_crs(state_bounds_shp_raw) # 5070 will need to convert to 102008
 
 
-# ---- wrangle data ----
-# wrangle
-ndfd_hist_pop12_data <- ndfd_hist_pop12_data_raw %>%
-  select(x_index, y_index, latitude, longitude, time_uct, time_nyc, pop12_value_perc, valid_period_hrs) %>%
-  mutate(latitude_m = latitude * 1000,
+# ---- wrangle pop12 data ----
+# initial clean up
+ndfd_pop12_data <- ndfd_pop12_data_raw %>%
+  dplyr::select(x_index, y_index, latitude, longitude, time_uct, time_nyc, pop12_value_perc, valid_period_hrs) %>%
+  dplyr::mutate(latitude_m = latitude * 1000,
          longitude_m = longitude * 1000)
-# ndfd_hist_qpf_data <- ndfd_hist_qpf_data_raw %>%
-#   select(x_index, y_index, latitude, longitude, time_uct, time_nyc, qpf_value_kmperm2, valid_period_hrs) %>%
-#   mutate(qpf_value_in = qpf_value_kmperm2 * (1/1000) * (100) * (1/2.54)) # convert to inches, density of water is 1000 kg/m3
 
-# select only 12 hr output
-ndfd_hist_pop12_data_12hr <- ndfd_hist_pop12_data %>%
-  filter(valid_period_hrs == 12) %>%
-  mutate(latitude_m = latitude * 1000,
-         longitude_m = longitude * 1000)
-# ndfd_hist_qpf_data_12hr <- ndfd_hist_qpf_data %>%
-#   filter(valid_period_hrs == 12)
 
+# ---- wrangle qpf data ----
+# initial clean up
+ndfd_qpf_data <- ndfd_qpf_data_raw %>%
+  dplyr::select(x_index, y_index, latitude, longitude, time_uct, time_nyc, qpf_value_kmperm2, valid_period_hrs) %>%
+  dplyr::mutate(latitude_m = latitude * 1000,
+         longitude_m = longitude * 1000,
+         qpf_value_in = qpf_value_kmperm2 * (1/1000) * (100) * (1/2.54)) # convert to inches, density of water is 1000 kg/m3
+
+
+# ---- convert tabular data to spatial data ----
 # define proj4 string for ndfd data
 ndfd_proj4 = "+proj=lcc +lat_1=25 +lat_2=25 +lat_0=25 +lon_0=-95 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +no_defs"
 # source: https://spatialreference.org/ref/sr-org/6825/
-
-# ndfd_proj4 = "+proj=lcc +lat_1=32 +lat_2=42 +lat_0=35 +lon_0=-80 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +datum=WGS84 +units=m +no_defs"
-
-# range(ndfd_hist_pop12_data$latitude)
-# 741.9331 to 1752.7349
-# range(ndfd_hist_pop12_data$longitude)
-# 878.7302 to 1859.0555
-
-# range(ndfd_hist_pop12_data$latitude_m)
-# 741933.1 to 1752734.9
-# range(ndfd_hist_pop12_data$longitude_m)
-# 878730.2 to 1859055.5
-
-# is this helpful?: https://gis.stackexchange.com/questions/345714/creating-custom-proj4-string-from-components
-# or this?: http://geotiff.maptools.org/proj_list/lambert_conic_conformal_2sp.html
-# 
 
 # define epsg and proj4 for N. America Albers projection (projecting to this)
 na_albers_proj4 <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 na_albers_epsg <- 102008
 
-# convert to spatial data
-ndfd_hist_pop12_data_albers_shp <- st_as_sf(ndfd_hist_pop12_data, coords = c("longitude_m", "latitude_m"), crs = ndfd_proj4, dim = "XY") #%>%
-  #st_transform(crs = na_albers_proj4) %>%
-  #st_set_crs(na_albers_epsg)
-st_crs(ndfd_hist_pop12_data_albers_shp)
-
-# ndfd_hist_qpf_data_albers_shp <- st_as_sf(ndfd_hist_qpf_data, coords = c("longitude", "latitude"), crs = ndfd_proj4, dim = "XY") %>%
-#   st_transform(crs = na_albers_proj4) %>%
-#   st_set_crs(na_albers_epsg)
-# st_crs(ndfd_hist_qpf_data_albers_shp)
-
-# calculate bounding box of ndfd data
-ndfd_pop12_data_bbox <- st_bbox(ndfd_hist_pop12_data_albers_shp)
-ndfd_pop12_data_bbox_shp <- st_as_sfc(ndfd_pop12_data_bbox, crs = ndfd_proj4) %>%
-  st_transform(crs = 4326)
-ndfd_pop12_data_bbox_shp <- st_as_sfc(ndfd_pop12_data_bbox, crs = na_albers_epsg)
-ndfd_pop12_data_bbox_shp
-# this isn't working for some reason....
-
-# try manually making a box
-ndfd_pop12_bounds <- data.frame(long = c(ndfd_pop12_data_bbox[1], ndfd_pop12_data_bbox[3], ndfd_pop12_data_bbox[3], ndfd_pop12_data_bbox[1]),
-                                lat = c(ndfd_pop12_data_bbox[4], ndfd_pop12_data_bbox[4], ndfd_pop12_data_bbox[2], ndfd_pop12_data_bbox[2]))
-ndfd_pop12_bounds_shp <- st_as_sf(ndfd_pop12_bounds, coords = c("long", "lat"), crs = ndfd_proj4, dim = "XY")  %>%
+# pop12
+# convert pop12 to spatial data
+ndfd_pop12_shp <- st_as_sf(ndfd_pop12_data, coords = c("longitude_m", "latitude_m"), crs = ndfd_proj4, dim = "XY") %>%
   st_transform(crs = na_albers_epsg)
+st_crs(ndfd_pop12_shp)
+
+# periods available
+unique(ndfd_pop12_shp$valid_period_hrs)
+
+# select 1-day
+ndfd_pop12_shp_1day <- ndfd_pop12_shp %>%
+  dplyr::filter(valid_period_hrs == 24)
+
+# select 2-day
+ndfd_pop12_shp_2day <- ndfd_pop12_shp %>%
+  dplyr::filter(valid_period_hrs == 48)
+
+# select 3-day
+ndfd_pop12_shp_3day <- ndfd_pop12_shp %>%
+  dplyr::filter(valid_period_hrs == 72)
 
 
+# qpf
+# convert qpf to spatial data
+ndfd_qpf_shp <- st_as_sf(ndfd_qpf_data, coords = c("longitude_m", "latitude_m"), crs = ndfd_proj4, dim = "XY") %>%
+  st_transform(crs = na_albers_epsg)
+st_crs(ndfd_qpf_shp)
 
-# ---- select mid atlantic state bounds ----
+# periods available
+unique(ndfd_qpf_shp$valid_period_hrs)
+
+# select 1-day
+ndfd_qpf_shp_1day <- ndfd_qpf_shp %>%
+  dplyr::filter(valid_period_hrs == 24)
+
+# select 2-day
+ndfd_qpf_shp_2day <- ndfd_qpf_shp %>%
+  dplyr::filter(valid_period_hrs == 48)
+
+# select 3-day
+ndfd_qpf_shp_3day <- ndfd_qpf_shp %>%
+  dplyr::filter(valid_period_hrs == 72)
+
+
+# ---- wrangle other spatial data ----
+# state bounds
+state_bounds_shp <- state_bounds_shp_raw %>%
+  st_transform(crs = na_albers_epsg)
+st_crs(state_bounds_shp)
+
 # list of mid atlantic states
-midatlan_states_list <- c("IN", "OH", "KY", "WV", "PA", "MD", "DE", "NJ", "VA", "TN", "NC", "SC", "GA", "AL", "TX", "LA", "MS", "FL")
+midatlan_states_list <- c("IN", "OH", "KY", "WV", "PA", "MD", "DE", "NJ", "VA", "TN", "NC", "SC", "GA", "AL")
 # source: https://www.weather.gov/images/mdl/midatlan.gif
 
 # select ndfd mid atlantic states bounds only
-midatlantic_bounds_shp <- state_bounds_albers_shp %>%
+midatlantic_bounds_shp <- state_bounds_shp %>%
   filter(STUSPS %in% midatlan_states_list)
-  
-#st_write(ndfd_pop12_bounds_shp, paste0(figure_path, "pop12_bbox.shp"))
-#st_write(midatlantic_bounds_shp, paste0(figure_path, "southeast_states.shp"))
 
-# ---- make data a grid ----
+# keep only geometry
+midatlantic_bounds_geom <- midatlantic_bounds_shp %>%
+  st_geometry()
 
-#st_make_grid()
-
-# ---- plot data ----
-# ggplot(data = ndfd_hist_pop12_data_12hr) +
-#   geom_point(aes(x = x_index, y = y_index, color = pop12_value_perc)) +
-#   scale_color_gradient(low = "white", high = "blue", na.value = "grey90")
-
-# plot pop12 data
-ggplot(data = ndfd_hist_pop12_data_12hr) +
-  geom_point(aes(x = longitude, y = latitude, color = pop12_value_perc)) +
-  scale_color_gradient(low = "white", high = "blue", na.value = "grey90", limits = c(0, 100))
-
-# plot qpf data
-#max(ndfd_hist_qpf_data$qpf_value_in, na.rm = TRUE)
-ggplot(data = ndfd_hist_qpf_data_12hr) +
-  geom_point(aes(x = longitude, y = latitude, color = qpf_value_in)) +
-  scale_color_gradient(low = "white", high = "blue", na.value = "grey90", limits = c(0, 1))
+# export mid atlantic states
+#st_write(midatlantic_bounds_shp, paste0(spatial_data_exort_path, "midatlantic_state_bounds.shp"))
 
 
-# ----- plot spatial data ----
-# plot state bounds
-pdf(file = paste0(figure_path, "test_1.pdf"), width = 11, height = 8.5)
-ggplot(data = ndfd_pop12_data_bbox_shp) +
-  geom_sf(data = midatlantic_bounds_shp) +
-  geom_sf(data = ndfd_pop12_data_bbox_shp, fill= NA, lwd = 2)
+# ---- calculate bounding box of ndfd data to check spatial bounds ----
+# keep pop12 1-day geometry
+ndfd_pop12_geom_1day <- ndfd_pop12_shp %>% 
+  dplyr::filter(valid_period_hrs == 24) %>% 
+  st_geometry()
+
+# keep qpf 1-day geometry
+ndfd_qpf_geom_1day <- ndfd_qpf_shp %>% 
+  dplyr::filter(valid_period_hrs == 24) %>% 
+  st_geometry()
+
+# calculate bounding box of ndfd data
+ndfd_pop12_bbox_1day <- st_bbox(ndfd_pop12_geom_1day)
+
+# convert to spatial object
+ndfd_pop12_bbox_shp_1day <- st_as_sfc(ndfd_pop12_bbox_1day, crs = na_albers_epsg) 
+st_crs(ndfd_pop12_bbox_shp_1day)
+
+# try manually making a spatial object from the bbox
+# ndfd_pop12_bbox_tab_1day <- data.frame(long = c(ndfd_pop12_bbox_1day[1], ndfd_pop12_bbox_1day[3], ndfd_pop12_bbox_1day[3], ndfd_pop12_bbox_1day[1]),
+#                                 lat = c(ndfd_pop12_bbox_1day[4], ndfd_pop12_bbox_1day[4], ndfd_pop12_bbox_1day[2], ndfd_pop12_bbox_1day[2]))
+# ndfd_pop12_bbox_shp_1day <- st_as_sf(ndfd_pop12_bbox_tab_1day, coords = c("long", "lat"), crs = na_albers_epsg, dim = "XY")
+# st_crs(ndfd_pop12_bbox_shp_1day)
+
+# plot to check
+pdf(file = paste0(figure_path, "spatial_extent_check_pop12.pdf"), width = 11, height = 8.5)
+ggplot() +
+  geom_sf(data = midatlantic_bounds_geom) +
+  geom_sf(data = ndfd_pop12_bbox_shp_1day, fill = NA, color = "red", lwd = 2)
 dev.off()
+# should look like: https://www.weather.gov/images/mdl/midatlan.gif
+# check!
+
+# export to check
+#st_write(ndfd_pop12_bbox_shp_1day, paste0(spatial_data_exort_path, "ndfd_pop12_bbox.shp"))
+
+
+
+# ---- make vector data into a raster ----
+# make empty pop12 raster
+ndfd_pop12_grid_1day <- raster(ncol = length(unique(ndfd_pop12_shp_1day$longitude)), 
+                               nrows = length(unique(ndfd_pop12_shp_1day$latitude)), 
+                               crs = na_albers_proj4,
+                               ext = extent(ndfd_pop12_shp_1day)) #, 
+                               #res = c(5000, 5000)) # b/c coordinates are in m this is 5km x 5km
+
+# make empty qpf raster
+ndfd_qpf_grid_1day <- raster(ncol = length(unique(ndfd_qpf_shp_1day$longitude)), 
+                               nrows = length(unique(ndfd_qpf_shp_1day$latitude)), 
+                               crs = na_albers_proj4,
+                               ext = extent(ndfd_qpf_shp_1day)) #, 
+                               #res = c(5000, 5000)) # b/c coordinates are in m this is 5km x 5km
+
+# ndfd_pop12_grid_1day <- ndfd_pop12_shp_1day %>%
+#   st_make_grid(cellsize = c(5000, 5000), what = "centers")
+#st_make_grid() isn't going to work b/c this doesn't let you keep a third value
+
+# rasterize pop12
+ndfd_pop12_grid_1day <- rasterize(ndfd_pop12_shp_1day, ndfd_pop12_grid_1day, field = ndfd_pop12_shp_1day$pop12_value_perc, fun = mean)
+
+
+# rasterize qpf
+ndfd_qpf_grid_1day <- rasterize(ndfd_qpf_shp_1day, ndfd_qpf_grid_1day, field = ndfd_qpf_shp_1day$qpf_value_in, fun = mean)
+
+# plot to check
+plot(ndfd_pop12_grid_1day)
+plot(ndfd_qpf_grid_1day)
+
+# export to check in qgis
+# writeRaster(ndfd_pop12_grid_1day, paste0(figure_path, "pop12_1day.tif"))
+# writeRaster(ndfd_qpf_grid_1day, paste0(figure_path, "qpf_1day.tif"))
+
+# help for editing maps in r: https://nceas.github.io/oss-lessons/spatial-data-gis-law/3-mon-intro-gis-in-r.html
+# help: https://gis.stackexchange.com/questions/273233/create-spatial-polygon-grid-from-spatial-points-in-r
+# more help: https://gis.stackexchange.com/questions/24588/converting-point-data-into-gridded-dataframe-for-histogram-analysis-using-r
+
+
+
+# ---- mask ndfd data ----
+
+
+
+# ---- calculate propability of closure ----
+
+
+
+# ---- write loop for 1-, 2-, 3-day calcs ----
+
+
+
+
+
+
+
 
