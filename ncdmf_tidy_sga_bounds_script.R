591044f43e0da82d67cb5b662d29b78bccd02058
sheila <sms493@cornell.edu>
2020-05-18 20:18:29 -0400

    tidy and summarize data

--- ncdmf_tidy_sga_bounds_script.R
+++ ncdmf_tidy_sga_bounds_script.R
@@ -21,35 +21,27 @@ library(tidyverse)
 library(sf)
 
 # set paths
-spatial_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/spatial/ncdmf_raw/"
+spatial_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/spatial/sheila_generated/sga_bounds/"
 
 # export path
-spatial_data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/spatial/sheila_generated/"
+# same as spatial_data_path for now
+spatial_data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/spatial/sheila_generated/sga_bounds/"
 
 
 # ---- 2. load data ----
 
-# shellfish growing area data
-sga_data_raw <- st_read(paste0(spatial_data_path, "SGA_Current_Classifications_may2019/SGA_Current_Classifications.shp"))
+# shellfish growing area data that's been initially cleaned (see ncdmf_init_tidy_sga_bounds_script.R)
+sga_data_raw <- st_read(paste0(spatial_data_path, "sga_bounds_raw_fixed.shp"))
 
 # columns
-# OBJECTID_1, OBJECTID_2, OBJECTID, OBJECTID_3, SGA_INDEX - not really sure what these are but think these are all artifacts from ArcGIS
+# OBJECTID_1 - not really sure what these are but think these are all artifacts from ArcGIS
 # REGION = region of coast (central, north, south) 
 # GROW_AREA = growing area (GA) code is a two digits code (e.g., letter + number like A1), first digit (letter) is HA_AREA, second digist (number) is HA_SUBAREA
 # DSHA_NAME = full written out name for location (e.g., Calabash Area)
-# DSHA_ID = like GA code but has dash between digits (e.g., A-1)
-# DSHA_AREA = first digit of GA (e.g., letter like A)
-# DSHA_CODE = like GA code but lower case letter (e.g., a1)
-# DSHA_LABEL = same as DSHA_ID
+# HA_NAME = full written out name for location given by HA_CODE (e.g., Calabash/Sunset Beach/Boneparte Creek Area)
 # HA_CLASS = describes status of GA (restricted, conditionally approved - open, conditionally approved - closed, approved)
 # HA_CLASSID = shorter version of HA_CLASS (R, CA-O, CA-C, APP)
-# HA_NAME = full written out name for location given by HA_CODE (e.g., Calabash/Sunset Beach/Boneparte Creek Area)
-# HA_NAMEID = like HA_CODE but with dashes and spaces (e.g., A-1 CA-C 1)
 # HA_STATUS = GA status (open or closed)
-# HA_AREA = first digit letter of GA (e.g., A), A through I
-# HA_SUBAREA = second digit number of GA (e.g., 1)
-# HA_CODE = like GA code but has additional information including status and subsub area (e.g., a1cac1)
-# HA_LABEL = same as HA_NAMEID
 # MAP_NAME = contributing watershed/river (61 unique values)
 # MAP_NUMBER = map number corresponding to MAP_NAME (51 unique values)
 # MAP_ID = number from MAP_NUMBER and letter (?)
@@ -63,47 +55,45 @@ sga_data_raw <- st_read(paste0(spatial_data_path, "SGA_Current_Classifications_m
 # UPDATED = date the area/data was updated?
 # ACRES = area of grow area in acres
 # SQ_MILES = area of grow area in sqare miles
-# SHAPE_Leng = ?
-# Shape_Le_1 = ?
-# GlobalID = ?
-# SHAPE_Le_2 =? 
-# GlobalID_2 = ?
-# Shape__Are = ?
-# Shape__Len = ?
-# GlobalID_3 = ?
-# Shape__A_1 = ?
-# Shape__L_1 = ?
 
-# other observations
-# 1. as far as I can tell DSHA_CODE, HA_NAMEID, HA_AREA, HA_SUBAREA all have similar info to GROW_AREA
-# 2. HA_LABEL,DSHA_LABEL, and MAP_ID have a lot of NA values (not filled in values) so might not be very helpful
-# 3. HA_CODE has a third digit (i.e., letters) that GROW_AREA does not have - will need this
-# 4. DSHA_NAME and HA_NAME are similar but looks like HA_NAME has more detail (going along with HA_NAMEID and HA_CODE)
-# 5. some of the MAP_NAME and MAP_NUMBER columns are NA values
-# 6. what's the details with the UPDATED column?
-# 7. sometimes HA_AREA and HA_SUBAREA are not given even though GROW_AREA is
-# 8. HA_AREA and HA_SUBAREA are never given if GROW_AREA is not given - basically they're not too helpful
 
-# ---- 3. tidy sga bounds ----
+# ---- 3. tidy sga bounds attribute data ----
 
 # tidy data
 sga_bounds <- sga_data_raw %>%
-  select(GROW_AREA, REGION, DSHA_NAME, HA_CLASS, HA_CLASSID, HA_NAME, MAP_NAME, COUNTY, JURISDCTN, WATER_DES, SURFACE, CREATOR) %>%
-  filter(is.na(GROW_AREA) != TRUE) %>% # delete rows where no ga given
   mutate(grow_area_class = if_else(HA_CLASSID == "APP", "approved",
                             if_else(HA_CLASSID == "CA-O" | HA_CLASSID == "CA-C", "cond_approved",
-                                    if_else(HA_CLASSID == "CSHA-P", "prohibited", "restricted")))) %>% # don't care if they're open or closed, just if they are approved, conditionally approve, prohibited, or restricted
-  select(grow_area = GROW_AREA, 
+                                    if_else(HA_CLASSID == "CSHA-P", "prohibited", "restricted"))), # don't care if they're open or closed, just if they are approved, conditionally approve, prohibited, or restricted
+         grow_area_trim = str_trim(GROW_AREA, side = "both"),
+         ha_area_fix = str_to_upper(str_sub(grow_area_trim, start = 1, end = 1)), # original HA_AREA has some NAs so generate from scratch
+         ha_subarea_fix = str_pad(str_sub(grow_area_trim, start = 2, end = -1), 2, side = "left", "0"), # original HA_SUBAREA has some NAs so generate from scratch
+         grow_area = paste0(ha_area_fix, ha_subarea_fix), 
+         ha_name_fix = if_else(is.na(HA_NAME) == TRUE, as.character(DSHA_NAME), as.character(HA_NAME))) %>% # if there's no name use general name (i.e., DSHA_NAME)
+  select(grow_area,
          grow_area_class, 
          region = REGION, 
          dsha_name = DSHA_NAME, 
          ha_name = HA_NAME,
+         ha_name_fix,
          map_name = MAP_NAME, 
          county = COUNTY, 
          jursidiction = JURISDCTN,
          water_desc = WATER_DES,
          surface = SURFACE,
-         creator = CREATOR)# reorder columns
+         creator = CREATOR) %>% # reorder columns
+  arrange(grow_area, grow_area_class, ha_name) %>%
+  mutate(row_id = seq(1:7033))
+
+# filter out row 5399 and fix (only row with NULL ha_name_fix)
+sga_bounds_row_5399_fix <- sga_bounds %>%
+  filter(row_id == 5399)
+sga_bounds_row_5399_fix$dsha_name <- "North River"
+sga_bounds_row_5399_fix$ha_name_fix <- "North River"
+
+# take out and add fixed version of row 5399 back
+sga_bounds <- sga_bounds %>%
+  filter(row_id != 5399) %>%
+  rbind(sga_bounds_row_5399_fix)
 
 # check validity of polygons
 sga_bounds$polygon_valid_check <- st_is_valid(sga_bounds)
@@ -112,60 +102,33 @@ sga_bounds$polygon_valid_check <- st_is_valid(sga_bounds)
 sga_bounds_invalid_tabular <- sga_bounds %>% 
   st_drop_geometry() %>%
   filter(polygon_valid_check == FALSE)
+# there should be no invalid polygons which is true, so, check!
 
-# list of sga's with invalid geometries
-sga_bounds_invalid_list <- unique(sga_bounds_invalid_tabular$grow_area)
-length(sga_bounds_invalid_list) #45
-
-# export each sga as a separate file (for manual fixing in QGIS)
-for (i in 1:length(sga_bounds_invalid_list)) {
-  sga_temp <- sga_bounds_invalid_list[i]
-  sga_select_shp_temp <- sga_bounds %>%
-    filter(grow_area == sga_temp)
-  sga_temp_path <- paste0(spatial_data_export_path, "sga_fixing/sga_", sga_temp, ".shp")
-  st_write(sga_select_shp_temp, sga_temp_path)
-}
-
-# list of completely valid sga's
-sga_bounds_all_comp <- data.frame(grow_area = unique(sga_bounds$grow_area))
-sga_bounds_invalid_comp <- data.frame(grow_area = unique(sga_bounds_invalid_list))
-sga_bounds_valid_comp <- anti_join(sga_bounds_all_comp, sga_bounds_invalid_comp)
-sga_bounds_valid_comp_list <- sga_bounds_valid_comp$grow_area
-
-# keep valid sga's together
-sga_bounds_valid <- sga_bounds %>%
-  filter(grow_area %in% sga_bounds_valid_comp_list)
-
-# export all sga's that are valid in one file
-st_write(sga_bounds_valid, paste0(spatial_data_export_path, "sga_fixing/sga_ok.shp"))
+# export
+st_write(sga_bounds, paste0(spatial_data_export_path, "sga_bounds_tidy.shp"))
 
 
-# filter by only polygons that are valid
-sga_bounds_valid <- sga_bounds %>%
-  filter(polygon_valid_check == TRUE)
+# ---- 4. tidy sga boundaries by sga and class ----
 
-test <- sga_bounds_valid %>%
-  group_by(grow_area, grow_area_class) %>%
-  summarise()
-# all other tabular data is missing tho!
+# summarize boundaries (equivalent to spatial dissolve)
+sga_class_bounds <- sga_bounds %>%
+  group_by(grow_area, grow_area_class, ha_name_fix) %>% # group by sga name and class
+  summarize() %>%
+  st_buffer(dist = 0.00001) # buffer by very small amount (~3ft buffer) to get clean boundary, distance units are in decimal degrees
 
-test_2 <- 
-
-# steps to get data into polygons (did this in qgis but could i do it in R?)
-# 1. group by ga and then ga class
-# 2. dissolve
-# 3. buffer with units as = 0.00001 (for WGS84 = EPSG 4326)
-# 4. project?
-# qgis scripting (in python) https://www.qgistutorials.com/en/docs/processing_python_scripts.html
-
-# qgis check invalid geometries: https://www.qgistutorials.com/en/docs/3/handling_invalid_geometries.html
-# do this for each ga?
-  
-# ---- 4. export tidied data ----
-
-st_write(sga_bounds, paste0(spatial_data_export_path, "sga_bounds/sga_bounds_r.shp"))
-st_write(sga_bounds_valid, paste0(spatial_data_export_path, "sga_bounds/sga_bounds_r_valid.shp"))
-st_write(test, paste0(spatial_data_export_path, "sga_bounds/sga_bounds_r_dissolve.shp"))
+# export data
+st_write(sga_class_bounds, paste0(spatial_data_export_path, "sga_class_bounds.shp"))
 
 
-  
+# ---- 5. tidy sga boundaries by sga (simple boundary line) ----
+
+# summarize boundaries (equivalent to spatial dissolve)
+sga_bounds_simple <- sga_bounds %>%
+  group_by(grow_area) %>% # group by sga name
+  summarize() %>%
+  st_buffer(dist = 0.00001) # distance units are in decimal degrees (~3ft buffer)
+
+# export data
+st_write(sga_bounds_simple, paste0(spatial_data_export_path, "sga_bounds_simple.shp"))
+
+
