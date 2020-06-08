5df5030bd645e3382fc5705d433be8983aaf2b8f
sheila <sms493@cornell.edu>
2020-06-08 19:31:47 -0400

    buffer cmu bounds

--- ncdmf_tidy_cmu_bounds_script.R
+++ ncdmf_tidy_cmu_bounds_script.R
@@ -3,7 +3,7 @@
 # script name: ncdmf_tidy_cmu_bounds_script.R
 # purpose of script: reformats the Conditional Management Units shapefile for downstream use
 # author: sheila saia
-# date created: 202004016
+# date created: 20200416
 # email: ssaia@ncsu.edu
 
 
@@ -32,6 +32,9 @@ spatial_data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shell
 # export path for rainfall threshold tabular data
 tabular_data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/sheila_generated/"
 
+# figure export path
+figure_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/results/figures/"
+
 # define epsg and proj4 for N. America Albers projection (projecting to this)
 na_albers_proj4 <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
 na_albers_epsg <- 102008
@@ -43,6 +46,7 @@ wgs84_epsg <- 4326
 # ---- 3. load data ----
 # cmu spatial data
 cmu_bounds_raw <- st_read(paste0(spatial_data_path, "cmu_bounds/cmu_bounds_fix_bs10.shp"))
+# the original data 
 
 # rainfall thresholds for cmu's
 rain_thresh_full_data_raw <- read_csv(paste0(tabular_data_path, "rainfall_thresholds.csv"), col_names = TRUE)
@@ -68,6 +72,28 @@ cmu_bounds_wgs84 <- cmu_bounds_albers %>%
 # export data
 st_write(cmu_bounds_albers, paste0(spatial_data_export_path, "cmu_bounds_albers.shp"))
 st_write(cmu_bounds_albers, paste0(spatial_data_export_path, "cmu_bounds_wgs84.shp"))
+
+
+# ---- calculate simple buffer around cmu bounds ----
+# cmu buffer
+cmu_bounds_buffer_albers <- cmu_bounds_albers %>%
+  st_convex_hull() %>% # for each cmu
+  summarize() %>% # dissolve cmu bounds
+  st_buffer(dist = 10000) %>% # buffer distance is in m so 10 * 1000m = 10km
+  st_convex_hull() # simple buffer
+
+pdf(paste0(figure_path, "cmu_bounds_buffer.pdf"), width = 11, height = 8.5)
+ggplot(data = cmu_bounds_buffer_albers) +
+  geom_sf()
+dev.off()
+
+# save a copy projected to wgs84
+cmu_bounds_buffer_wgs84 <- cmu_bounds_buffer_albers %>%
+  st_transform(crs = wgs84_epsg)
+
+# export
+st_write(cmu_bounds_buffer_albers, paste0(spatial_data_export_path, "cmu_bounds_buffer_albers.shp"))
+st_write(cmu_bounds_buffer_wgs84, paste0(spatial_data_export_path, "cmu_bounds_buffer_wgs84.shp"))
 
 
 # ---- 5. simplify rainfall threshhold data ----
