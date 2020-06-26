93f8c7ce6db90ed8c394a55a265d9d392393e908
sheila <sms493@cornell.edu>
2020-06-26 17:18:31 -0400

    initial commit

---
+++ ncdmf_get_latest_lease_data_script.R
@@ -0,0 +1,17 @@
+# ---- script header ----
+# script name: ncdmf_get_latest_lease_data_script.R
+# purpose of script:
+# author:
+# date created: 20200626
+# email:
+
+
+# ---- notes ----
+# notes:
+ 
+
+# ---- to do ----
+# to do list
+
+
+# ----
