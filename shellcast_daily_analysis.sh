be59d31509fdf72e27eee0444d30b80d38803c30
sheila <sms493@cornell.edu>
2020-07-16 12:43:29 -0400

    saved outputs to text file

--- shellcast_daily_analysis.sh
+++ shellcast_daily_analysis.sh
@@ -5,10 +5,13 @@
 # email: ssaia@ncsu.edu
 
 # step 1
-python ndfd_get_forecast_data_script.py
+# python ndfd_get_forecast_data_script.py | tee ...data/tabular/output/terminal_data/python_output_$(date '+%Y%m%d').txt
+python ndfd_get_forecast_data_script.py | tee /Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/web_app_data/tabular/outputs/terminal_data/01_get_forecast_out_$(date '+%Y%m%d').txt
 
 # step 2
-Rscript ndfd_convert_df_to_raster_script.R
+# Rscript ndfd_convert_df_to_raster_script.R | tee ...data/tabular/outputs/terminal_data/convert_df_out_$(date '+%Y%m%d').txt
+Rscript ndfd_convert_df_to_raster_script.R | tee /Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/web_app_data/tabular/outputs/terminal_data/02_convert_df_out_$(date '+%Y%m%d').txt
 
 # step 3
-Rscript ndfd_analyze_forecast_data_script.R
+# Rscript ndfd_analyze_forecast_data_script.R | tee ...data/tabular/outputs/terminal_data/analyze_out_$(date '+%Y%m%d').txt
+Rscript ndfd_analyze_forecast_data_script.R | tee /Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/web_app_data/tabular/outputs/terminal_data/03_analyze_out_$(date '+%Y%m%d').txt
