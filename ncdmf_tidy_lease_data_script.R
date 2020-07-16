d2992e6c380eb66d4e0d0d6ce29079cab6440feb
sheila <sms493@cornell.edu>
2020-07-16 12:40:12 -0400

    added lease metadata and fixed file paths

--- ncdmf_tidy_lease_data_script.R
+++ ncdmf_tidy_lease_data_script.R
@@ -27,32 +27,44 @@
 # ---- to do ----
 # to do list
 
-# TODO get this read to run in real-time
-# TODO error if no no recent lease data available
 # TODO need to export file with lease_id and geo bounds for database
 # TODO save all spatial data outputs to geojson (to reduce storage demands)
+# TODO (wishlist) use here package
 
 
-# ---- 1. install packages ----
-# only need to install these once
-# install.packages("tidyverse")
-# install.packages("sf")
-# install.packages("geojsonsf")
+# ---- 1. install and load packages as necessary ----
+# packages
+packages <- c("tidyverse", "sf", "geojsonsf")
+
+# install and load
+for (package in packages) {
+  if (! package %in% installed.packages()) {
+    install.packages(package, dependencies = TRUE)
+  }
+  library(package, character.only = TRUE)
+}
 
 
-# ---- 2. load libraries ----
-library(tidyverse)
-library(sf)
-library(geojsonsf)
-# library(here)
+# ---- 2. define base paths ----
+# base path to data
+# data_base_path = "...analysis/data/" # set this and uncomment!
+data_base_path = "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/web_app_data/" 
 
 
 # ---- 3. defining paths and projections ----
-# path to raw data
-lease_raw_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/web_app_data/spatial/ncdmf_raw/lease_bounds_raw/"
+# path to raw lease spatial inputs
+lease_data_spatial_input_path <- paste0(data_base_path, "spatial/outputs/ncdmf_data/lease_bounds_raw/")
 
-# export path
-lease_export_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/web_app_data/spatial/generated/lease_bounds/"
+# path to cmu buffer spatial inputs
+cmu_spatial_data_input_path <- paste0(data_base_path, "spatial/inputs/ncdmf_data/cmu_bounds/")
+
+# path to rainfall threshold tabular inputs
+rainfall_thresh_tabular_data_input_path <- paste0(data_base_path, "tabular/inputs/ncdmf_rainfall_thresholds/")
+
+
+# path to lease spatial outputs
+lease_data_spatial_output_path <-  paste0(data_base_path, "spatial/outputs/ncdmf_data/")
+
 
 # define epsg and proj4 for N. America Albers projection (projecting to this)
 na_albers_proj4 <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
@@ -63,40 +75,73 @@ wgs84_epsg <- 4326
 wgs84_proj4 <- "+proj=longlat +datum=WGS84 +no_defs"
 
 
-# ---- 4. load in latest least data ----
+# ---- 4a. load in lease and rainfall threshold data (without dates) ----
+# spatial data
+# use latest date to read in most recent data
+lease_bounds_raw <- st_read(paste0(lease_data_spatial_input_path, "lease_bounds_raw.shp"))
+
+# check projection
+# st_crs(lease_bounds_raw) # epsg = 2264
+
+# cmu data
+cmu_bounds_albers <- st_read(paste0(cmu_spatial_data_input_path, "cmu_bounds_albers.shp"))  %>%
+  st_set_crs(na_albers_epsg) # epsg code wasn't assigned if this code isn't included
+
+# check projection
+# st_crs(cmu_bounds_albers) # epsg = 102008
+
+
+# tabular data
+# rainfall thresholds
+rainfall_thresh_data <- read_csv(paste0(rainfall_thresh_tabular_data_input_path, "rainfall_thresholds.csv"))
+
+
+# ---- 4b. load in latest least data (with dates) ----
 # list files in lease_bounds_raw
-lease_files <- list.files(lease_raw_data_path, pattern = "*.shp") # if there is a pop12 dataset there's a qpf dataset
+# lease_files <- list.files(lease_data_spatial_input_path, pattern = "*.shp") # if there is a pop12 dataset there's a qpf dataset
 # lease_files <- c(lease_files, "leases_20200401.shp") # to test with multiple
 
 # pull out date strings
-lease_file_dates_str <- gsub("leases_", "", gsub(".shp", "", lease_files))
+# lease_file_dates_str <- gsub("leases_", "", gsub(".shp", "", lease_files))
 
 # convert date strings to dates
-lease_file_dates <- lubridate::ymd(lease_file_dates_str)
+# lease_file_dates <- lubridate::ymd(lease_file_dates_str)
 
 # get today's date
-today_date_uct <- lubridate::today(tzone = "UCT")
+# today_date_uct <- lubridate::today(tzone = "UCT")
 
 # calcualte difference
-diff_file_dates <- as.numeric(today_date_uct - lease_file_dates) # in days
+# diff_file_dates <- as.numeric(today_date_uct - lease_file_dates) # in days
 
 # find position of smallest difference
-latest_date_uct <- lease_file_dates[diff_file_dates == min(diff_file_dates)]
+# latest_date_uct <- lease_file_dates[diff_file_dates == min(diff_file_dates)]
 
 # convert to string
-latest_date_uct_str <- strftime(latest_date_uct, format = "%Y%m%d")
+# latest_date_uct_str <- strftime(latest_date_uct, format = "%Y%m%d")
 
 # need if statement that if length(date_check) < 1 then don't run this script
 
 # use latest date to read in most recent data
-lease_data_raw <- st_read(paste0(lease_raw_data_path, "leases_", latest_date_uct_str, ".shp"))
+# lease_bounds_raw <- st_read(paste0(lease_raw_data_path, "leases_", latest_date_uct_str, ".shp"))
 
 # check projection
-# st_crs(lease_data_raw) # epsg = 2264
+# st_crs(lease_bounds_raw) # epsg = 2264
+
+# sga data
+# sga_bounds_albers <- st_read(paste0(sga_spatial_data_input_path, "sga_bounds_simple_albers.shp"))  %>%
+#   st_set_crs(na_albers_epsg) # epsg code wasn't assigned if this code isn't included
+
+# check projection
+# st_crs(sga_bounds_albers) # epsg = 102008
+
+
+# tabular data
+# rainfall thresholds
+# rainfall_thresh_data <- read_csv(paste0(rainfall_thresh_tabular_data_input_path, "rainfall_thresholds.csv"))
 
 
 # ---- 5. project and tidy up lease data ----
-lease_data_albers <- lease_data_raw %>%
+lease_data_albers <- lease_bounds_raw %>%
   st_transform(crs = na_albers_epsg) %>% # project
   dplyr::select(lease_id = ProductNbr, # tidy
                 owner = Owner,
@@ -110,17 +155,57 @@ lease_data_albers <- lease_data_raw %>%
 # st_crs(lease_data_albers)
 
 
-# ---- 6. find centroids of leases ----
+# ---- 7. find centroids of leases ----
 # calculate centroids of leases for map pins
 lease_data_centroid_albers <- lease_data_albers %>%
   st_centroid()
 
 
-# ---- 7. project data ----
+# ---- 6. add sga and rainfall depths to lease data ----
+# trim cmu data
+cmu_bounds_albers_sel <- cmu_bounds_albers %>%
+  dplyr::select(HA_CLASS, rain_in, rain_lab)
+
+# trim rainfall threshold data
+rainfall_thresh_data_sel <- rainfall_thresh_data %>%
+  dplyr::select(HA_CLASS, grow_area)
+
+# join lease and cmu data by geometry
+# joining by cmu and not sga because each sga has mutiple cmu's within and therefore multiple rainfall thresholds
+lease_data_centroid_albers_join <- lease_data_centroid_albers %>%
+  st_intersection(cmu_bounds_albers_sel) %>%
+  dplyr::left_join(rainfall_thresh_data_sel, by = "HA_CLASS") %>%
+  dplyr::group_by(lease_id) %>%
+  dplyr::slice(which.min(rain_in)) %>%
+  #dplyr::group_by(lease_id, rain_in) %>%
+  #dplyr::count() %>%
+  dplyr::select(lease_id:county, grow_area, rain_thresh_in = rain_in, rain_thresh_desc = rain_lab)
+
+# NOTE!
+# in the case that the centroid of the lease is at the edge of two growing areas then this code will take
+# the minimum rainfall threshold, as of 20200716 it does not look like this is an issue but it cold be in
+# the future
+# for now there are ~ 20 (513-483 = 20) leases that have centroids which are not in cmu's and those are dropped
+
+# save tabular data from lease_data_centroid_albers_join to the lease polygons
+lease_data_centroids_join_no_geom <- lease_data_centroid_albers_join %>%
+  st_drop_geometry() %>%
+  dplyr::select(lease_id, grow_area:rain_thresh_desc)
+
+# join to polygon dataset
+lease_data_albers_join <- lease_data_albers %>%
+  dplyr::left_join(lease_data_centroids_join_no_geom, by = "lease_id")
+
+# resave centroids b/c lease_data_albers_join has full lease dataset
+lease_data_centroids_albers_final <- lease_data_albers_join %>%
+  st_centroid()
+
+
+# ---- 8. project data ----
 # project data and centroids to wgs84 projection
-lease_data_wgs94 <- lease_data_albers %>%
+lease_data_wgs94 <- lease_data_albers_join %>%
   st_transform(crs = wgs84_epsg)
-lease_data_centroid_wgs94 <- lease_data_centroid_albers %>%
+lease_data_centroid_wgs94 <- lease_data_centroids_albers_final %>%
   st_transform(crs = wgs84_epsg)
 
 # project data to geojson file type (need this for the web app)
@@ -128,26 +213,30 @@ lease_data_wgs94_geojson <- sf_geojson(lease_data_wgs94, atomise = FALSE, simpli
 lease_data_centroid_wgs94_geojson <- sf_geojson(lease_data_centroid_wgs94, atomise = FALSE, simplify = TRUE, digits = 5)
 
 
-# ---- 8. export data ----
+# ---- 9. export data ----
 # export data as shape file for record keeping
-st_write(lease_data_albers, paste0(lease_export_data_path, "leases_albers/leases_albers_", latest_date_uct_str, ".shp"))
-st_write(lease_data_centroid_albers, paste0(lease_export_data_path, "leases_centroids_albers/leases_centroids_albers_", latest_date_uct_str, ".shp"))
+# st_write(lease_data_albers_join, paste0(lease_data_spatial_output_path, "lease_bounds_albers/lease_bounds_albers_", latest_date_uct_str, ".shp")) # includes date in file name
+# st_write(lease_data_centroids_albers_final, paste0(lease_data_spatial_output_path, "lease_centroids_albers/leases_centroids_albers_", latest_date_uct_str, ".shp")) # includes date in file name
+st_write(lease_data_albers_join, paste0(lease_data_spatial_output_path, "lease_bounds_albers/lease_bounds_albers.shp"), delete_layer = TRUE)
+st_write(lease_data_centroids_albers_final, paste0(lease_data_spatial_output_path, "lease_centroids_albers/leases_centroids_albers.shp"), delete_layer = TRUE)
 
 # export data as geojson for web app
-write_file(lease_data_wgs94_geojson, paste0(lease_export_data_path, "leases_wgs84/leases_wgs84_", latest_date_uct_str, ".geojson"))
-write_file(lease_data_centroid_wgs94_geojson, paste0(lease_export_data_path, "leases_centroids_wgs84/leases_centroid_wgs84_", latest_date_uct_str, ".geojson"))
+# write_file(lease_data_wgs94_geojson, paste0(lease_data_spatial_output_path, "lease_bounds_wgs84/lease_bounds_wgs84_", latest_date_uct_str, ".geojson")) # includes date in file name
+# write_file(lease_data_centroid_wgs94_geojson, paste0(lease_data_spatial_output_path, "lease_centroids_wgs84/leases_centroids_wgs84_", latest_date_uct_str, ".geojson")) # includes date in file name
+write_file(lease_data_wgs94_geojson, paste0(lease_data_spatial_output_path, "lease_bounds_wgs84/lease_bounds_wgs84.geojson"))
+write_file(lease_data_centroid_wgs94_geojson, paste0(lease_data_spatial_output_path, "lease_centroids_wgs84/leases_centroids_wgs84.geojson"))
 
 
-# ---- 9. extra code ----
+# ---- extra code ----
 # select leases (ignore in production)
-# st_write(lease_data_albers, paste0(lease_export_data_path, "leases_select_albers.shp"))
-# st_write(lease_data_centroid_albers, paste0(lease_export_data_path, "leases_select_centroid_albers.shp"))
-# write_file(lease_data_wgs94_geojson, paste0(lease_export_data_path, "leases_select_wgs84.geojson"))
-# write_file(lease_data_centroid_wgs94_geojson, paste0(lease_export_data_path, "leases_select_centroid_wgs84.geojson"))
+# st_write(lease_data_albers, paste0(lease_data_spatial_output_path, "leases_select_albers.shp"))
+# st_write(lease_data_centroid_albers, paste0(lease_data_spatial_output_path, "leases_select_centroid_albers.shp"))
+# write_file(lease_data_wgs94_geojson, paste0(lease_data_spatial_output_path, "leases_select_wgs84.geojson"))
+# write_file(lease_data_centroid_wgs94_geojson, paste0(lease_data_spatial_output_path, "leases_select_centroid_wgs84.geojson"))
 
 # raw lease data to wgs84 and geojson (for stanton)
-# lease_data_raw_wgs84 <- lease_data_raw %>%
+# lease_bounds_raw_wgs84 <- lease_bounds_raw %>%
 #   st_transform(crs = wgs84_epsg)
-# st_crs(lease_data_raw_wgs84)
-# lease_data_raw_wgs84_geojson <- sf_geojson(lease_data_raw_wgs84, atomise = FALSE, simplify = TRUE, digits = 5)
-# write_file(lease_data_raw_wgs84_geojson, paste0(lease_data_path, "leases_select_raw_wgs84.geojson"))
+# st_crs(lease_bounds_raw_wgs84)
+# lease_bounds_raw_wgs84_geojson <- sf_geojson(lease_bounds_raw_wgs84, atomise = FALSE, simplify = TRUE, digits = 5)
+# write_file(lease_bounds_raw_wgs84_geojson, paste0(lease_data_path, "leases_select_raw_wgs84.geojson"))
