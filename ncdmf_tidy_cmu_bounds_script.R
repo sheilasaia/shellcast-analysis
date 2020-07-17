61017089bcf9e6f5b155a99d4cb67fa28a179eb5
sheila <sms493@cornell.edu>
2020-07-17 15:33:57 -0400

    fixed paths and package namespace issues

--- ncdmf_tidy_cmu_bounds_script.R
+++ ncdmf_tidy_cmu_bounds_script.R
@@ -15,26 +15,32 @@
 # to do list
 
 
-# ---- 1. load libraries ----=
+# ---- 1. load libraries -----
 # load libraries
 library(tidyverse)
 library(sf)
 
 
-# ---- 2. set paths and define projections ----
-# set paths
-spatial_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/spatial/sheila_generated/"
-tabular_data_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/sheila_generated/ncdmf_rainfall_thresholds/"
+# ---- 2. define base paths ----
+# base path to data
+# data_base_path = "...analysis/data/" # set this and uncomment!
+data_base_path = "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/web_app_data/" 
 
 
-# export path for cmu bounds spatial data
-spatial_data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/spatial/sheila_generated/cmu_bounds/"
+# ---- 3. use base paths and define projections ----
+# path to cmu spatial inputs
+cmu_spatial_data_input_path <- paste0(data_base_path, "spatial/inputs/ncdmf_data/cmu_bounds/")
 
-# export path for rainfall threshold tabular data
-tabular_data_export_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/tabular/sheila_generated/ncdmf_rainfall_thresholds/"
+# path to rainfall threshold tabular inputs
+rainfall_thresh_tabular_data_input_path <- paste0(data_base_path, "tabular/inputs/ncdmf_rainfall_thresholds/")
 
-# figure export path
-figure_path <- "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/results/figures/"
+
+# path to cmu spatial outputs
+cmu_spatial_data_output_path <- paste0(data_base_path, "spatial/inputs/ncdmf_data/cmu_bounds/")
+
+# path to rainfall threshold tabular outputs
+rainfall_thresh_tabular_data_output_path <- paste0(data_base_path, "tabular/inputs/ncdmf_rainfall_thresholds/")
+
 
 # define epsg and proj4 for N. America Albers projection (projecting to this)
 na_albers_proj4 <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"
@@ -44,9 +50,9 @@ na_albers_epsg <- 102008
 wgs84_epsg <- 4326
 
 
-# ---- 3. load data ----
+# ---- 4. load data ----
 # cmu spatial data
-cmu_bounds_raw <- st_read(paste0(spatial_data_path, "cmu_bounds/cmu_bounds_fix_bs10obx9a_valid.shp"))
+cmu_bounds_raw <- st_read(paste0(cmu_spatial_data_input_path, "cmu_bounds_fix_bs10obx9a_valid.shp"))
 
 # the file "cmu_bounds_fix_bs10obx9a_valid.shp" came from "Conditional_Management_Units.shp" from ncdmf
 # i used the 'multipart split' editing tool to rename one of the two BS_1 cmu's
@@ -65,15 +71,15 @@ cmu_bounds_raw <- st_read(paste0(spatial_data_path, "cmu_bounds/cmu_bounds_fix_b
 # saved fixed (and checked) file in QGIS as 'sga_bounds_raw_valid_wgs84.shp' in the 'data/spatial/sheila_generated/sga_bounds/' directory
 
 # rainfall thresholds for cmu's
-rain_thresh_full_data_raw <- read_csv(paste0(tabular_data_path, "rainfall_thresholds.csv"), col_names = TRUE)
+rain_thresh_full_data_raw <- read_csv(paste0(rainfall_thresh_tabular_data_input_path, "rainfall_thresholds.csv"), col_names = TRUE)
 
 
-# ---- 4. tidy rainfall thresholds and join to cmu data ----
+# ---- 5. tidy rainfall thresholds and join to cmu data ----
 # select only columns we need
 rain_thresh_data <- rain_thresh_full_data_raw %>%
   dplyr::select(HA_CLASS, rain_in = rainfall_threshold_in, rain_lab = rainfall_threshold_class) %>%
   distinct()
-length(unique(rain_thresh_data$HA_CLASS))
+# length(unique(rain_thresh_data$HA_CLASS))
 # 144 unique HA_CLASS values 
 
 # check unique HA_CLASS values in cmu_bounds_raw
@@ -89,9 +95,9 @@ cmu_bounds_raw_join <- cmu_bounds_raw %>%
 # it checks!
 
 
-# ---- 4. check spatial data projections ----
+# ---- 6. check spatial data projections ----
 # check cmu data
-st_crs(cmu_bounds_raw_join)
+# st_crs(cmu_bounds_raw_join)
 # epsg 32119 (nc nad83)
 
 # project to na albers
@@ -101,17 +107,17 @@ cmu_bounds_albers <- cmu_bounds_raw_join %>%
 # it checks!
 
 # project to wgs84
-cmu_bounds_wgs84 <- cmu_bounds_albers %>%
+cmu_bounds_wgs84 <- cmu_bounds_raw_join %>%
   st_transform(crs = wgs84_epsg)
 # st_crs(cmu_bounds_wgs84)
 # it checks!
 
 # export data
-st_write(cmu_bounds_albers, paste0(spatial_data_export_path, "cmu_bounds_albers.shp"), append = FALSE)
-st_write(cmu_bounds_albers, paste0(spatial_data_export_path, "cmu_bounds_wgs84.shp"), append = FALSE)
+st_write(cmu_bounds_albers, paste0(cmu_spatial_data_output_path, "cmu_bounds_albers.shp"), delete_layer = TRUE)
+st_write(cmu_bounds_wgs84, paste0(cmu_spatial_data_output_path, "cmu_bounds_wgs84.shp"), delete_layer = TRUE)
 
 
-# ---- calculate simple buffer around cmu bounds ----
+# ---- 7. calculate simple buffer around cmu bounds ----
 # cmu buffer
 cmu_bounds_buffer_albers <- cmu_bounds_albers %>%
   st_convex_hull() %>% # for each cmu
@@ -129,19 +135,17 @@ cmu_bounds_buffer_wgs84 <- cmu_bounds_buffer_albers %>%
   st_transform(crs = wgs84_epsg)
 
 # export
-st_write(cmu_bounds_buffer_albers, paste0(spatial_data_export_path, "cmu_bounds_buffer_albers.shp"), append = FALSE)
-st_write(cmu_bounds_buffer_wgs84, paste0(spatial_data_export_path, "cmu_bounds_buffer_wgs84.shp"), append = FALSE)
+st_write(cmu_bounds_buffer_albers, paste0(cmu_spatial_data_output_path, "cmu_bounds_10kmbuf_albers.shp"), delete_layer = TRUE)
+st_write(cmu_bounds_buffer_wgs84, paste0(cmu_spatial_data_output_path, "cmu_bounds_10kmbuf_wgs84.shp"), delete_layer = TRUE)
 
 
-# ---- 5. simplify rainfall threshhold data ----
+# ---- 8. simplify rainfall threshhold data ----
 # simplify rainfall thresholds for sharing with Andy
 rain_thresh_short <- rain_thresh_full_data_raw %>%
   dplyr::select(-grow_area) %>%
   distinct()
 
 # export data
-write_csv(rain_thresh_short, paste0(tabular_data_export_path, "rainfall_thresholds_no_ga.csv"))
+write_csv(rain_thresh_short, paste0(rainfall_thresh_tabular_data_output_path, "rainfall_thresholds_no_ga.csv"))
 
 
-
- 
