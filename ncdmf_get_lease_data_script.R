cb30d1bdbdbf937876f98424a6a7dfa8dd92b3b4
sheila <sms493@cornell.edu>
2020-08-27 11:51:40 -0400

    export wgs84 raw lease data

--- ncdmf_get_lease_data_script.R
+++ ncdmf_get_lease_data_script.R
@@ -15,5 +15,11 @@
 
 
 # ----
+lease_bounds_raw <- st_read(paste0(lease_data_spatial_input_path, "lease_bounds_raw.shp"))
+lease_bounds_raw_wgs84 <- lease_bounds_raw %>%
+  st_transform(wgs84_epsg)
+st_crs(lease_bounds_raw_wgs84)
+st_write(lease_bounds_raw_wgs84, paste0(lease_data_spatial_input_path, "lease_bounds_raw_wgs84.shp"))
 
+# export wgs84 toooo!
 # export to "/Users/sheila/Documents/bae_shellcast_project/shellcast_analysis/data/web_app_data/spatial/ncdmf_raw/lease_bounds_raw/"
